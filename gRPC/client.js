d2797ab9743c50c1b58cdf3b9d318b6cc98a948d
Knot <knot131-wr@hotmail.com>
2020-09-16 21:11:01 +0700

    add c

--- gRPC/client.js
+++ gRPC/client.js
@@ -5,14 +5,6 @@ var booksProto = grpc.load('books.proto');
 var client = new booksProto.books.BookService('127.0.0.1:50051', 
   grpc.credentials.createInsecure());
 
-client.list({}, function(error, books) {
-  if (error)
-    console.log('Error: ', error);
-  else
-    console.log(books);
-});
-
-
 function printResponse(error, response) {
   if (error)
     console.log('Error: ', error);
